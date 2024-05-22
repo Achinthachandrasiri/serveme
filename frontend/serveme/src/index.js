d3d91129d158269ee5f3580add0a9c3d80694a8a
Achinthachandrasiri <achinthahost3000@gmail.com>
2024-05-22 15:22:18 +0300

    Update index.js

--- frontend/serveme/src/index.js
+++ frontend/serveme/src/index.js
@@ -108,6 +108,7 @@ const UserViewNewGig = () => (
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <Router basename="/serveme">
     <Router>
         <Routes>
           <Route path="/" element={<Login />} />
